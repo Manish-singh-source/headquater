dfa26401b4a8b3c20073ccf987bd1d24886484e6
manish <manish@technofra.com>
2025-06-11 13:02:54 +0530

    change side bar

--- sidebar.php
+++ sidebar.php
@@ -36,7 +36,7 @@
             <ul>
               <li><a href="staff.php"><i class="material-icons-outlined">arrow_right</i>Staff</a>
               </li>
-              <li><a href="role.php"><i class="material-icons-outlined">arrow_right</i>Role</a>
+              <li><a href="index2.php"><i class="material-icons-outlined">arrow_right</i>Role</a>
               </li>
             </ul>
           </li>
@@ -47,6 +47,14 @@
               <div class="menu-title">Customers</div>
             </a>
           </li>
+          <li>
+            <a href="vendor.php">
+              <div class="parent-icon"><i class="material-icons-outlined">people</i>
+              </div>
+              <div class="menu-title">Vendor</div>
+            </a>
+          </li>
+         
           <li>
             <a href="Warehouse.php">
               <div class="parent-icon"><i class="material-icons-outlined">home</i>
@@ -88,254 +96,7 @@
               </li>
             </ul>
           </li>
-          <li class="menu-label">UI Elements</li>
-          <li>
-            <a href="cards.php">
-              <div class="parent-icon"><i class="material-icons-outlined">inventory_2</i>
-              </div>
-              <div class="menu-title">Cards</div>
-            </a>
-          </li>
-          
-          <li>
-            <a href="javascript:;" class="has-arrow">
-              <div class="parent-icon"><i class="material-icons-outlined">shopping_bag</i>
-              </div>
-              <div class="menu-title">eCommerce</div>
-            </a>
-            <ul>
-              <li><a href="products.php"><i class="material-icons-outlined">arrow_right</i>Products</a>
-              </li>
-              <li><a href="customer.php"><i class="material-icons-outlined">arrow_right</i>Customers</a>
-              </li>
-              <li><a href="vendor.php"><i class="material-icons-outlined">arrow_right</i>Vendor</a>
-              </li>
-              <li><a href="ecommerce-customer-details.php"><i class="material-icons-outlined">arrow_right</i>Customer Details</a>
-              </li>
-              <li><a href="ecommerce-orders.php"><i class="material-icons-outlined">arrow_right</i>Orders</a>
-              </li>
-              <li><a href="ecommerce-order-details.php"><i class="material-icons-outlined">arrow_right</i>Order Details</a>
-              </li>
-            </ul>     
-          </li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">card_giftcard</i>
-              </div>
-              <div class="menu-title">Components</div>
-            </a>
-            <ul>
-              <li><a href="component-alerts.php"><i class="material-icons-outlined">arrow_right</i>Alerts</a>
-              </li>
-              <li><a href="component-accordions.php"><i class="material-icons-outlined">arrow_right</i>Accordions</a>
-              </li>
-              <li><a href="component-badges.php"><i class="material-icons-outlined">arrow_right</i>Badges</a>
-              </li>
-              <li><a href="component-buttons.php"><i class="material-icons-outlined">arrow_right</i>Buttons</a>
-              </li>
-              <li><a href="component-carousels.php"><i class="material-icons-outlined">arrow_right</i>Carousels</a>
-              </li>
-              <li><a href="component-media-object.php"><i class="material-icons-outlined">arrow_right</i>Media
-                  Objects</a>
-              </li>
-              <li><a href="component-modals.php"><i class="material-icons-outlined">arrow_right</i>Modals</a>
-              </li>
-              <li><a href="component-navs-tabs.php"><i class="material-icons-outlined">arrow_right</i>Navs & Tabs</a>
-              </li>
-              <li><a href="component-navbar.php"><i class="material-icons-outlined">arrow_right</i>Navbar</a>
-              </li>
-              <li><a href="component-paginations.php"><i class="material-icons-outlined">arrow_right</i>Pagination</a>
-              </li>
-              <li><a href="component-popovers-tooltips.php"><i class="material-icons-outlined">arrow_right</i>Popovers
-                  & Tooltips</a>    
-              </li>
-              <li><a href="component-progress-bars.php"><i class="material-icons-outlined">arrow_right</i>Progress</a>
-              </li>
-              <li><a href="component-spinners.php"><i class="material-icons-outlined">arrow_right</i>Spinners</a>
-              </li>
-              <li><a href="component-notifications.php"><i
-                    class="material-icons-outlined">arrow_right</i>Notifications</a>
-              </li>
-              <li><a href="component-avtars-chips.php"><i class="material-icons-outlined">arrow_right</i>Avatrs &
-                  Chips</a>
-              </li>
-              <li><a href="component-typography.php"><i class="material-icons-outlined">arrow_right</i>Typography</a>
-               </li>
-               <li><a href="component-text-utilities.php"><i class="material-icons-outlined">arrow_right</i>Utilities</a>
-               </li>
-            </ul>
-          </li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">view_agenda</i>
-              </div>
-              <div class="menu-title">Icons</div>
-            </a>
-            <ul>
-              <li><a href="icons-line-icons.php"><i class="material-icons-outlined">arrow_right</i>Line Icons</a>
-              </li>
-              <li><a href="icons-boxicons.php"><i class="material-icons-outlined">arrow_right</i>Boxicons</a>
-              </li>
-              <li><a href="icons-feather-icons.php"><i class="material-icons-outlined">arrow_right</i>Feather
-                  Icons</a>
-              </li>
-            </ul>
-          </li>
-          <li class="menu-label">Forms & Tables</li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">toc</i>
-              </div>
-              <div class="menu-title">Forms</div>
-            </a>
-            <ul>
-              <li><a href="form-elements.php"><i class="material-icons-outlined">arrow_right</i>Form Elements</a>
-              </li>
-              <li><a href="form-input-group.php"><i class="material-icons-outlined">arrow_right</i>Input Groups</a>
-              </li>
-              <li><a href="form-radios-and-checkboxes.php"><i class="material-icons-outlined">arrow_right</i>Radios &
-                  Checkboxes</a>
-              </li>
-              <li><a href="form-layouts.php"><i class="material-icons-outlined">arrow_right</i>Forms Layouts</a>
-              </li>
-              <li><a href="form-validations.php"><i class="material-icons-outlined">arrow_right</i>Form Validation</a>
-              </li>
-              <li><a href="form-wizard.php"><i class="material-icons-outlined">arrow_right</i>Form Wizard</a>
-              </li>
-              <li><a href="form-file-upload.php"><i class="material-icons-outlined">arrow_right</i>File Upload</a>
-              </li>
-              <li><a href="form-date-time-pickes.php"><i class="material-icons-outlined">arrow_right</i>Date
-                  Pickers</a>
-              </li>
-              <li><a href="form-select2.php"><i class="material-icons-outlined">arrow_right</i>Select2</a>
-              </li>
-              <li><a href="form-repeater.php"><i class="material-icons-outlined">arrow_right</i>Form Repeater</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">api</i>
-              </div>
-              <div class="menu-title">Tables</div>
-            </a>
-            <ul>
-              <li><a href="table-basic-table.php"><i class="material-icons-outlined">arrow_right</i>Basic Table</a>
-              </li>
-              <li><a href="table-datatable.php"><i class="material-icons-outlined">arrow_right</i>Data Table</a>
-              </li>
-            </ul>
-          </li>
-          <li class="menu-label">Pages</li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">lock</i>
-              </div>
-              <div class="menu-title">Authentication</div>
-            </a>
-            <ul>
-              <li><a class="has-arrow" href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Basic</a>
-                <ul>
-                  <li><a href="auth-basic-login.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Login</a></li>
-                  <li><a href="auth-basic-register.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Register</a></li>
-                  <li><a href="auth-basic-forgot-password.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Forgot Password</a></li>
-                  <li><a href="auth-basic-reset-password.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Reset Password</a></li>
-                </ul>
-              </li>
-              <li><a class="has-arrow" href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Cover</a>
-                <ul>
-                  <li><a href="auth-cover-login.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Login</a></li>
-                  <li><a href="auth-cover-register.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Register</a></li>
-                  <li><a href="auth-cover-forgot-password.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Forgot Password</a></li>
-                  <li><a href="auth-cover-reset-password.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Reset Password</a></li>
-                </ul>
-              </li>
-              <li><a class="has-arrow" href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Boxed</a>
-                  <ul>
-                    <li><a href="auth-boxed-login.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Login</a></li>
-                    <li><a href="auth-boxed-register.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Register</a></li>
-                    <li><a href="auth-boxed-forgot-password.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Forgot Password</a></li>
-                    <li><a href="auth-boxed-reset-password.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>Reset Password</a></li>
-                  </ul>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="user-profile.php">
-              <div class="parent-icon"><i class="material-icons-outlined">person</i>
-              </div>
-              <div class="menu-title">User Profile</div>
-            </a>
-          </li>
-          <li>
-            <a href="timeline.php">
-              <div class="parent-icon"><i class="material-icons-outlined">join_right</i>
-              </div>
-              <div class="menu-title">Timeline</div>
-            </a>
-          </li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">report_problem</i>
-              </div>
-              <div class="menu-title">Pages</div>
-            </a>
-            <ul>
-              <li><a href="pages-error-404.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>404
-                  Error</a>
-              </li>
-              <li><a href="pages-error-505.php" target="_blank"><i class="material-icons-outlined">arrow_right</i>505
-                  Error</a>
-              </li>
-              <li><a href="pages-coming-soon.php" target="_blank"><i
-                    class="material-icons-outlined">arrow_right</i>Coming Soon</a>
-              </li>
-              <li><a href="pages-starter-page.php" target="_blank"><i
-                    class="material-icons-outlined">arrow_right</i>Blank Page</a> 
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="faq.php">
-              <div class="parent-icon"><i class="material-icons-outlined">help_outline</i>
-              </div>
-              <div class="menu-title">FAQ</div>
-            </a>
-          </li>
-          <li>
-            <a href="pricing-table.php">
-              <div class="parent-icon"><i class="material-icons-outlined">sports_football</i>
-              </div>
-              <div class="menu-title">Pricing</div>
-            </a>
-          </li>
-          <li class="menu-label">Charts & Maps</li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">fitbit</i>
-              </div>
-              <div class="menu-title">Charts</div>
-            </a>
-            <ul>
-              <li><a href="charts-apex-chart.php"><i class="material-icons-outlined">arrow_right</i>Apex</a>
-              </li>
-              <li><a href="charts-chartjs.php"><i class="material-icons-outlined">arrow_right</i>Chartjs</a>
-              </li>   
-            </ul>
-          </li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">sports_football</i>
-              </div>
-              <div class="menu-title">Maps</div>
-            </a>
-            <ul>
-              <li><a href="map-google-maps.php"><i class="material-icons-outlined">arrow_right</i>Google Maps</a>
-              </li>
-              <li><a href="map-vector-maps.php"><i class="material-icons-outlined">arrow_right</i>Vector Maps</a>
-              </li>
-            </ul>
-          </li>
+         
           <li class="menu-label">Others</li>
           <li>
             <a class="has-arrow" href="javascript:;">
