bab107f461e4567d20883437ea0e8e539d6d2051
saurabh <support@technofra.com>
2025-06-12 16:26:34 +0530

    added warehouse sidebar nav tabs

--- sidebar.php
+++ sidebar.php
@@ -1,126 +1,153 @@
    <!--start sidebar-->
    <aside class="sidebar-wrapper" data-simplebar="true">
-    <div class="sidebar-header">
-      <div class="logo-icon">
-        <a href="index.php">
-        <img src="assets/images/logo-icon.png" class="logo-img" alt="">
-        </a>
-      </div>
+     <div class="sidebar-header">
+       <div class="logo-icon">
+         <a href="index.php">
+           <img src="assets/images/logo-icon.png" class="logo-img" alt="">
+         </a>
+       </div>
        <a href="index.php">
-      <div class="logo-name flex-grow-1">
-        <h5 class="mb-0">HEADQUATERS</h5>
-      </div>
-      </a>
-      <div class="sidebar-close">
-        <span class="material-icons-outlined">close</span>
-      </div>
-    </div>  
-    <div class="sidebar-nav">
-        <!--navigation-->
-        <ul class="metismenu" id="sidenav">
-          <li>
-            <a href="index.php">
-               <div class="parent-icon"><i class="material-icons-outlined">home</i>
-              </div>
-              <div class="menu-title">Dashboard</div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:;" class="has-arrow">
-              <div class="parent-icon"><i class="material-icons-outlined">key</i>
-              </div>
-              <div class="menu-title">Access Control</div>
-            </a>
-            <ul>
-              <li><a href="staff.php"><i class="material-icons-outlined">arrow_right</i>Staff</a>
-              </li>
-              <li><a href="role.php"><i class="material-icons-outlined">arrow_right</i>Role</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="ecommerce-customers.php">
-              <div class="parent-icon"><i class="material-icons-outlined">people</i>
-              </div>
-              <div class="menu-title">Customers</div>
-            </a>
-          </li>
-          <li>
-            <a href="vendor.php">
-              <div class="parent-icon"><i class="material-icons-outlined">storefront</i>
-              </div>
-              <div class="menu-title">Vendor</div>
-            </a>
-          </li>
-         
-          <li>
-            <a href="Warehouse.php">
-              <div class="parent-icon"><i class="material-icons-outlined">store</i>
-              </div>
-              <div class="menu-title">Warehouse</div>
-            </a>
-          </li>
-          <li>
-            <a href="order.php">
-              <div class="parent-icon"><i class="material-icons-outlined">home</i>
-              </div>
-              <div class="menu-title">Order</div>
-            </a>
-          </li>
-          <li>
-            <a href="products.php">
-              <div class="parent-icon"><i class="material-icons-outlined">shopping_bag</i>
-              </div>
-              <div class="menu-title">Products</div>
-            </a>
-          </li>  
-          <li>
-            <a href="assign-order.php">
-              <div class="parent-icon"><i class="material-icons-outlined">handshake</i>
-              </div>
-              <div class="menu-title">Assign Order</div>
-            </a>
-          </li>  
-          
-        
-          <li class="menu-label">Others</li>
-          <li>
-            <a class="has-arrow" href="javascript:;">
-              <div class="parent-icon"><i class="material-icons-outlined">face_5</i>
-              </div>
-              <div class="menu-title">Menu Levels</div>
-            </a>
-            <ul>
-              <li><a class="has-arrow" href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Level
-                  One</a>
-                <ul>
-                  <li><a class="has-arrow" href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Level
-                      Two</a>
-                    <ul>
-                      <li><a href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Level Three</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="javascrpt:;">
-              <div class="parent-icon"><i class="material-icons-outlined">description</i>
-              </div>
-              <div class="menu-title">Documentation</div>
-            </a>
-          </li>
-          <li>
-            <a href="javascrpt:;">
-              <div class="parent-icon"><i class="material-icons-outlined">support</i>
-              </div>
-              <div class="menu-title">Support</div>
-            </a>
-          </li>
-         </ul>
-        <!--end navigation-->
-    </div>
-  </aside>
-<!--end sidebar-->
+         <div class="logo-name flex-grow-1">
+           <h5 class="mb-0">HEADQUATERS</h5>
+         </div>
+       </a>
+       <div class="sidebar-close">
+         <span class="material-icons-outlined">close</span>
+       </div>
+     </div>
+     <div class="sidebar-nav">
+       <!--navigation-->
+       <ul class="metismenu" id="sidenav">
+         <li>
+           <a href="index.php">
+             <div class="parent-icon"><i class="material-icons-outlined">home</i>
+             </div>
+             <div class="menu-title">Dashboard</div>
+           </a>
+         </li>
+         <li>
+           <a href="javascript:;" class="has-arrow">
+             <div class="parent-icon"><i class="material-icons-outlined">key</i>
+             </div>
+             <div class="menu-title">Access Control</div>
+           </a>
+           <ul>
+             <li><a href="staff.php"><i class="material-icons-outlined">arrow_right</i>Staff</a>
+             </li>
+             <li><a href="role.php"><i class="material-icons-outlined">arrow_right</i>Role</a>
+             </li>
+           </ul>
+         </li>
+         <li>
+           <a href="ecommerce-customers.php">
+             <div class="parent-icon"><i class="material-icons-outlined">people</i>
+             </div>
+             <div class="menu-title">Customers</div>
+           </a>
+         </li>
+         <li>
+           <a href="vendor.php">
+             <div class="parent-icon"><i class="material-icons-outlined">storefront</i>
+             </div>
+             <div class="menu-title">Vendor</div>
+           </a>
+         </li>
+
+         <li>
+           <a href="order.php">
+             <div class="parent-icon"><i class="material-icons-outlined">home</i>
+             </div>
+             <div class="menu-title">Order</div>
+           </a>
+         </li>
+         <li>
+           <a href="products.php">
+             <div class="parent-icon"><i class="material-icons-outlined">shopping_bag</i>
+             </div>
+             <div class="menu-title">Products</div>
+           </a>
+         </li>
+         <li>
+           <a href="assign-order.php">
+             <div class="parent-icon"><i class="material-icons-outlined">handshake</i>
+             </div>
+             <div class="menu-title">Assign Order</div>
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <div class="parent-icon"><i class="material-icons-outlined">store</i>
+             </div>
+             <div class="menu-title">Invoices</div>
+           </a>
+         </li>
+         <li class="menu-label">Warehouse</li>
+         <li>
+           <a href="Warehouse.php">
+             <div class="parent-icon"><i class="material-icons-outlined">store</i>
+             </div>
+             <div class="menu-title">Warehouses</div>
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <div class="parent-icon"><i class="material-icons-outlined">store</i>
+             </div>
+             <div class="menu-title">Packaging List</div>
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <div class="parent-icon"><i class="material-icons-outlined">store</i>
+             </div>
+             <div class="menu-title">Tickets</div>
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <div class="parent-icon"><i class="material-icons-outlined">store</i>
+             </div>
+             <div class="menu-title">Ready To Ship</div>
+           </a>
+         </li>
+         <li class="menu-label">Others</li>
+         <li>
+           <a class="has-arrow" href="javascript:;">
+             <div class="parent-icon"><i class="material-icons-outlined">face_5</i>
+             </div>
+             <div class="menu-title">Menu Levels</div>
+           </a>
+           <ul>
+             <li><a class="has-arrow" href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Level
+                 One</a>
+               <ul>
+                 <li><a class="has-arrow" href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Level
+                     Two</a>
+                   <ul>
+                     <li><a href="javascript:;"><i class="material-icons-outlined">arrow_right</i>Level Three</a>
+                     </li>
+                   </ul>
+                 </li>
+               </ul>
+             </li>
+           </ul>
+         </li>
+         <li>
+           <a href="javascrpt:;">
+             <div class="parent-icon"><i class="material-icons-outlined">description</i>
+             </div>
+             <div class="menu-title">Documentation</div>
+           </a>
+         </li>
+         <li>
+           <a href="javascrpt:;">
+             <div class="parent-icon"><i class="material-icons-outlined">support</i>
+             </div>
+             <div class="menu-title">Support</div>
+           </a>
+         </li>
+       </ul>
+       <!--end navigation-->
+     </div>
+   </aside>
+   <!--end sidebar-->
