6506e33e9b733af478407354626ffbec75fabd2f
Roshan <support@technofra.com>
2025-06-14 12:42:37 +0530

    update profile section

--- header.php
+++ header.php
@@ -317,16 +317,16 @@
             <hr class="dropdown-divider">
             <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="user-profile.php"><i
               class="material-icons-outlined">person_outline</i>Profile</a>
-            <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
-              class="material-icons-outlined">local_bar</i>Setting</a>
-            <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
+            <!-- <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
+              class="material-icons-outlined">local_bar</i>Setting</a> -->
+            <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="index.php"><i
               class="material-icons-outlined">dashboard</i>Dashboard</a>
-            <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
+            <!-- <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
               class="material-icons-outlined">account_balance</i>Earning</a>
               <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
-                class="material-icons-outlined">cloud_download</i>Downloads</a>
+                class="material-icons-outlined">cloud_download</i>Downloads</a> -->
             <hr class="dropdown-divider">
-            <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
+            <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="login.php"><i
             class="material-icons-outlined">power_settings_new</i>Logout</a>
           </div>
         </li>
