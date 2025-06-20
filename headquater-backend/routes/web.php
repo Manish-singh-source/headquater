b764ecdb0aedeb14ed4f0e0827bd777706b3a995
saurabh <support@technofra.com>
2025-06-20 17:07:17 +0530

    added authentication system

--- headquater-backend/routes/web.php
+++ headquater-backend/routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
 
-Route::get('/login', [AuthController::class, 'loginCustomer']);
+Route::get('/login', [AuthController::class, 'loginCustomer'])->name('login');
+Route::post('/login', [AuthController::class, 'loginAuthCheckCustomerData'])->name('login.auth.check');
+Route::get('/register', [AuthController::class, 'registerCustomer'])->name('register');
+Route::post('/register', [AuthController::class, 'registerCustomerData'])->name('register.store');
+
