34b1c0218c22f61b988170f68e8b2384bd4b38c1
manish <manish@technofra.com>
2025-06-20 17:10:40 +0530

    add dashboard page

--- headquater-backend/routes/web.php
+++ headquater-backend/routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
 
 Route::get('/login', [AuthController::class, 'loginCustomer']);
 
