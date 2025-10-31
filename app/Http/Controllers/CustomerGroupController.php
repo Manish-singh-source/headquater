e53300e2c09e48300c24b2b44b439e13bada7d50
saurabh <support@technofra.com>
2025-10-31 15:57:37 +0530

    fixing issues

--- app/Http/Controllers/CustomerGroupController.php
+++ app/Http/Controllers/CustomerGroupController.php
@@ -13,7 +13,6 @@ use Spatie\SimpleExcel\SimpleExcelReader;
 
 class CustomerGroupController extends Controller
 {
-    //
     public function index()
     {
         $customerGroups = CustomerGroup::with('customerGroupMembers')->get();
@@ -26,28 +25,28 @@ class CustomerGroupController extends Controller
         return view('customerGroups.create');
     }
 
-    // Storing Group and it's customers
+    // Storing Group and its customers
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'csv_file' => 'required',
+            'csv_file' => 'required|file|mimes:csv,txt',
         ]);
 
         if ($validator->fails()) {
-            return back()->with($validator)->withInput();
+            return back()->withErrors($validator)->withInput();
         }
 
         $file = $request->file('csv_file');
-        if (! $file) {
-            return redirect()->back()->with(['csv_file' => 'Please upload a CSV file.']);
+        if (!$file) {
+            return redirect()->back()->withErrors(['csv_file' => 'Please upload a CSV file.']);
         }
 
         DB::beginTransaction();
 
         try {
             // 1. Create the customer group
-            $customerGroup = new CustomerGroup;
+            $customerGroup = new CustomerGroup();
             $customerGroup->name = $request['name'];
             $customerGroup->save();
 
@@ -59,70 +58,58 @@ class CustomerGroupController extends Controller
             $insertCount = 0;
 
             foreach ($reader->getRows() as $record) {
-                // $customer = Customer::where('client_name', $record['Client Name'])->first();
-                // $keywords = preg_split('/[\s\-]+/', $record['Shipping Address'], -1, PREG_SPLIT_NO_EMPTY);
-                // $query = DB::table('customers');
-
-                // $query->where(function ($q) use ($keywords) {
-                //     foreach ($keywords as $word) {
-                //         $q->orWhere('shipping_address', 'like', "%{$word}%");
-                //     }
-                // });
-
-                // $customer = $query->first();
-                if (! isset($record['Facility Name']) || empty($record['Facility Name'])) {
+                if (!isset($record['Facility Name']) || empty($record['Facility Name'])) {
                     throw new \Exception('Facility Name is required');
                 }
 
                 $customer = Customer::where('facility_name', $record['Facility Name'])->first();
 
-                if (! $customer) {
+                if (!$customer) {
                     // 2. Insert individual customer
                     $customer = Customer::create([
-                        'facility_name' => $record['Facility Name'] ?? '',
-                        'client_name' => $record['Client Name'] ?? '',
-                        'contact_name' => $record['Contact Name'] ?? '',
-                        'email' => $record['Email'] ?? '',
-                        'contact_no' => $record['Contact No'] ?? '',
-                        'company_name' => $record['Company Name'] ?? '',
-                        'gstin' => $record['GSTIN'] ?? '',
-                        'pan' => $record['PAN'] ?? '',
-                        'gst_treatment' => $record['GST Treatment'] ?? '',
-                        'private_details' => $record['Private Details'] ?? '',
-                        'billing_address' => $record['Billing Address'] ?? '',
-                        'billing_country' => $record['Billing Country'] ?? '',
-                        'billing_state' => $record['Billing State'] ?? '',
-                        'billing_city' => $record['Billing City'] ?? '',
-                        'billing_zip' => $record['Billing Zip'] ?? '',
+                        'facility_name'    => $record['Facility Name'] ?? '',
+                        'client_name'      => $record['Client Name'] ?? '',
+                        'contact_name'     => $record['Contact Name'] ?? '',
+                        'email'            => $record['Email'] ?? '',
+                        'contact_no'       => $record['Contact No'] ?? '',
+                        'company_name'     => $record['Company Name'] ?? '',
+                        'gstin'            => $record['GSTIN'] ?? '',
+                        'pan'              => $record['PAN'] ?? '',
+                        'gst_treatment'    => $record['GST Treatment'] ?? '',
+                        'private_details'  => $record['Private Details'] ?? '',
+                        'billing_address'  => $record['Billing Address'] ?? '',
+                        'billing_country'  => $record['Billing Country'] ?? '',
+                        'billing_state'    => $record['Billing State'] ?? '',
+                        'billing_city'     => $record['Billing City'] ?? '',
+                        'billing_zip'      => $record['Billing Zip'] ?? '',
                         'shipping_address' => $record['Shipping Address'] ?? '',
                         'shipping_country' => $record['Shipping Country'] ?? '',
-                        'shipping_state' => $record['Shipping State'] ?? '',
-                        'shipping_city' => $record['Shipping City'] ?? '',
-                        'shipping_zip' => $record['Shipping Zip'] ?? '',
+                        'shipping_state'   => $record['Shipping State'] ?? '',
+                        'shipping_city'    => $record['Shipping City'] ?? '',
+                        'shipping_zip'     => $record['Shipping Zip'] ?? '',
                     ]);
                 } else {
-                    // update customer
                     $customer->update([
-                        'facility_name' => $record['Facility Name'] ?? '',
-                        'client_name' => $record['Client Name'] ?? '',
-                        'contact_name' => $record['Contact Name'] ?? '',
-                        'email' => $record['Email'] ?? '',
-                        'contact_no' => $record['Contact No'] ?? '',
-                        'company_name' => $record['Company Name'] ?? '',
-                        'gstin' => $record['GSTIN'] ?? '',
-                        'pan' => $record['PAN'] ?? '',
-                        'gst_treatment' => $record['GST Treatment'] ?? '',
-                        'private_details' => $record['Private Details'] ?? '',
-                        'billing_address' => $record['Billing Address'] ?? '',
-                        'billing_country' => $record['Billing Country'] ?? '',
-                        'billing_state' => $record['Billing State'] ?? '',
-                        'billing_city' => $record['Billing City'] ?? '',
-                        'billing_zip' => $record['Billing Zip'] ?? '',
-                        'shipping_address' => $record['Shipping Address'] ?? '',
-                        'shipping_country' => $record['Shipping Country'] ?? '',
-                        'shipping_state' => $record['Shipping State'] ?? '',
-                        'shipping_city' => $record['Shipping City'] ?? '',
-                        'shipping_zip' => $record['Shipping Zip'] ?? '',
+                        'facility_name'    => $record['Facility Name'] ?? $customer->facility_name,
+                        'client_name'      => $record['Client Name'] ?? $customer->client_name,
+                        'contact_name'     => $record['Contact Name'] ?? $customer->contact_name,
+                        'email'            => $record['Email'] ?? $customer->email,
+                        'contact_no'       => $record['Contact No'] ?? $customer->contact_no,
+                        'company_name'     => $record['Company Name'] ?? $customer->company_name,
+                        'gstin'            => $record['GSTIN'] ?? $customer->gstin,
+                        'pan'              => $record['PAN'] ?? $customer->pan,
+                        'gst_treatment'    => $record['GST Treatment'] ?? $customer->gst_treatment,
+                        'private_details'  => $record['Private Details'] ?? $customer->private_details,
+                        'billing_address'  => $record['Billing Address'] ?? $customer->billing_address,
+                        'billing_country'  => $record['Billing Country'] ?? $customer->billing_country,
+                        'billing_state'    => $record['Billing State'] ?? $customer->billing_state,
+                        'billing_city'     => $record['Billing City'] ?? $customer->billing_city,
+                        'billing_zip'      => $record['Billing Zip'] ?? $customer->billing_zip,
+                        'shipping_address' => $record['Shipping Address'] ?? $customer->shipping_address,
+                        'shipping_country' => $record['Shipping Country'] ?? $customer->shipping_country,
+                        'shipping_state'   => $record['Shipping State'] ?? $customer->shipping_state,
+                        'shipping_city'    => $record['Shipping City'] ?? $customer->shipping_city,
+                        'shipping_zip'     => $record['Shipping Zip'] ?? $customer->shipping_zip,
                     ]);
                 }
 
@@ -137,11 +124,11 @@ class CustomerGroupController extends Controller
 
             if ($insertCount === 0) {
                 DB::rollBack();
-
-                return redirect()->back()->with(['csv_file' => 'No valid data found in the CSV file.']);
+                return redirect()->back()->withErrors(['csv_file' => 'No valid data found in the CSV file.']);
             }
 
             DB::commit();
+
             // Log activity
             activity()
                 ->performedOn($customerGroup)
@@ -153,8 +140,7 @@ class CustomerGroupController extends Controller
             return redirect()->route('customer.groups.index')->with('success', 'CSV file imported successfully. Group and customers created.');
         } catch (\Exception $e) {
             DB::rollBack();
-
-            return redirect()->back()->with(['error' => 'Something went wrong: Please Make Sure File has Facility Name Column Filled.' . $e->getMessage()]);
+            return redirect()->back()->withErrors(['error' => 'Something went wrong: Please make sure the file has Facility Name column filled. ' . $e->getMessage()]);
         }
     }
 
@@ -179,19 +165,22 @@ class CustomerGroupController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return back()->with($validator)->withInput();
+            return back()->withErrors($validator)->withInput();
         }
 
         $customerGroup = CustomerGroup::find($id);
-        $customerGroup->name = $request->name;
-        $customerGroup->save();
 
         if ($customerGroup) {
+            $oldAttributes = $customerGroup->getOriginal();
+
+            $customerGroup->name = $request->name;
+            $customerGroup->save();
+
             activity()
                 ->performedOn($customerGroup)
                 ->causedBy(Auth::user())
                 ->withProperties([
-                    'old' => $customerGroup->getPrevious(),
+                    'old' => $oldAttributes,
                     'new' => $customerGroup->getChanges(),
                 ])
                 ->event('updated')
@@ -203,37 +192,35 @@ class CustomerGroupController extends Controller
         return redirect()->route('customer.groups.index')->with('info', 'No changes made to Group Name.');
     }
 
-    // Deleting Group of customers and it's related customers
+    // Deleting Group of customers and its related customers
     public function destroy($id)
     {
-        if (! $id) {
+        if (!$id) {
             return redirect()->route('customer.groups.index')->with('error', 'Invalid Group ID.');
         }
         try {
             $customerGroup = CustomerGroup::findOrFail($id);
             $customerGroup->delete();
 
-            if ($customerGroup) {
-                activity()
-                    ->performedOn($customerGroup)
-                    ->causedBy(Auth::user())
-                    ->event('deleted')
-                    ->log('Customer Group Deleted');
+            activity()
+                ->performedOn($customerGroup)
+                ->causedBy(Auth::user())
+                ->event('deleted')
+                ->log('Customer Group deleted');
 
-                return redirect()->route('customer.groups.index')->with('success', 'Successfully Deleted Group');
-            }
+            return redirect()->route('customer.groups.index')->with('success', 'Successfully Deleted Group');
         } catch (\Exception $e) {
-            return redirect()->route('customer.groups.index')->with('error', 'You Can Not Delete This Group. You have to delete Sales Orders first.');
+            return redirect()->route('customer.groups.index')->with('error', 'You cannot delete this group. You have to delete sales orders first.');
         }
     }
 
     public function toggleStatus(Request $request)
     {
-        $customer = CustomerGroup::findOrFail($request->id);
-        $customer->status = $request->status;
-        $customer->save();
+        $group = CustomerGroup::findOrFail($request->id);
+        $group->status = $request->status;
+        $group->save();
 
-        if ($customer) {
+        if ($group) {
             return response()->json(['success' => true]);
         }
 
@@ -242,19 +229,21 @@ class CustomerGroupController extends Controller
 
     public function deleteSelected(Request $request)
     {
-        if (! $request->ids) {
+        if (!$request->ids) {
             return redirect()->back()->with('error', 'No customer groups selected for deletion.');
         }
 
         try {
             $ids = is_array($request->ids) ? $request->ids : explode(',', $request->ids);
-            $group = CustomerGroup::destroy($ids);
+            $deleted = CustomerGroup::destroy($ids);
 
-            if ($group) {
+            if ($deleted) {
                 return redirect()->back()->with('success', 'Selected customer groups deleted successfully.');
+            } else {
+                return redirect()->back()->with('error', 'No groups deleted.');
             }
         } catch (\Exception $e) {
-            return redirect()->route('customer.groups.index')->with('error', 'You Can Not Delete This Group. You have to delete Sales Orders first.');
+            return redirect()->route('customer.groups.index')->with('error', 'You cannot delete this group. You have to delete sales orders first.');
         }
     }
 }
