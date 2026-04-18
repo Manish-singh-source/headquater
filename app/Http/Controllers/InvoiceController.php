f938bec6837ca0db865681f29c49b640f7e8b8c5
saurabh <support@technofra.com>
2026-04-18 18:20:30 +0530

    fixed

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -1599,7 +1599,7 @@ class InvoiceController extends Controller
             'distance' => $distance,
         ]);
 
-        return 254;
+        return 0;
     }
 
     private function getDistance($source, $destination, $token)
