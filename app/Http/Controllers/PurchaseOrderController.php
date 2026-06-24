3d8d6bc67ff2366885910d008e338a01435be22b
saurabh <support@technofra.com>
2026-06-24 17:36:29 +0530

    added total quantity in purchase order

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -331,7 +331,7 @@ class PurchaseOrderController extends Controller
         $purchaseOrders = PurchaseOrder::with(['purchaseOrderProducts', 'vendorPI', 'salesOrder'])
             ->withSum('purchaseOrderProducts', 'ordered_quantity')
             ->withCount('purchaseOrderProducts')->latest()->get();
-
+        // dd($purchaseOrders);
         return view('purchaseOrder.index', compact('purchaseOrders'));
     }
 
